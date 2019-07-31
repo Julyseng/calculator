428037207bc464036e5b23bd6fae99e21ebe3fb3
Julyseng Lahpai <julyseng@gmail.com>
2019-08-01 09:47:32 +1200

    Add pseudocode to sample solution

--- sample-solution/calculator.js
+++ sample-solution/calculator.js
@@ -3,6 +3,8 @@ var numArray = []
 let display = document.getElementById('display')
 let isPreviousResult = false
 
+// listening for a click event, and getting a button value
+
 listen()
 
 function listen () {
@@ -12,24 +14,36 @@ function listen () {
 function getButtonValue () {
   let button = event.target.value
   if (!isNaN(button) || button === '.') {
+    // if button is not number or a . then execute number button function
     number(button)
   } else if (button === 'AC') {
+    // if the button is AC then execute the all clear function
     allClear()
   } else if (button === 'CE') {
+    // if the button is CE then execute the clear function
     clear()
   } else if (button === '=') {
+    // otherwise if button is equal to then execute calculate function
     calculate()
   } else {
+    // or store the number
     storeNumber(button)
   }
 }
 
+// declaring number function with button parameter
 function number (button) {
   if (button === '.' && numString.includes('.')) {
     return
+    // if button is . and numString includes a . then stop executing
   } else if (numString.charAt(0) === '0' && numString.length === 1 && button === '0') {
     return
+    {/* if the position of character 0 is equal to 0 
+      and if the length of the string is 1
+      and the button is 0
+    */}
   } else {
+    
     if (isPreviousResult === true){
       numString = ''
       isPreviousResult = false
